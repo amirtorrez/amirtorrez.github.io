6758a36ffd8ca65048af768803bb859049b41a8b
Amir Torrez <8184888+amirtorrez@users.noreply.github.com>
2018-03-07 17:44:43 -0600

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -55,3 +55,16 @@ $(document).ready(function() {
     mainClass: 'mfp-fade'
   });
 });
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/84FAF2C3.asc.txt',
+        '/acercade',
+        '/blog/etiquetas',
+        '/primer-entrada'
+      ]);
+    })
+  );
+});
