392b1cfd30293223f846884e925a817028d17994
Amir Torrez <8184888+amirtorrez@users.noreply.github.com>
2018-03-07 18:38:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-self.addEventListener('install', function(event) {
+/*self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
@@ -8,9 +8,9 @@ self.addEventListener('install', function(event) {
       ]);
     })
   );
-});
+});*/
 
-/*self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
 	caches.match(event.request).then(function(response) {
 		return response || fetch(event.request).then(function(r) {
 			caches.open('v1').then(function(cache) {
@@ -21,4 +21,4 @@ self.addEventListener('install', function(event) {
 	}).catch(function() {
 		return caches.match('/offline.html');
 	});
-});*/
+});
