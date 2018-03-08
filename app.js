dffd9a045913c9488db49a928b23a54f0b19665b
Amir Torrez <8184888+amirtorrez@users.noreply.github.com>
2018-03-07 18:25:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
+        '/',
         '/84FAF2C3.asc.txt',
         '/offline.html'
       ]);
