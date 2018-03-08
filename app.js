15bf8eb12c1c21b5a132f9ed273d054ac04373a0
Amir Torrez <8184888+amirtorrez@users.noreply.github.com>
2018-03-07 18:32:40 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match("/offline.html")
-  );
+caches.match(event.request).then(function(response) {
+  return response || fetch(event.request).then(function(r) {
+    caches.open('v1').then(function(cache) {
+      cache.put(event.request, r);
+    });
+    return r.clone();
+  });
+}).catch(function() {
+  return caches.match('/offline.html');
 });
 
 self.addEventListener('install', function(event) {
